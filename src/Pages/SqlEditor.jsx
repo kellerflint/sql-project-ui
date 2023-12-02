c2438662050d37835b02253c2d6fedafd6112d64
James Motherwell <motherwell.james@student.greenriver.edu>
2023-12-02 12:16:35 -0800

    Refactor results table

--- src/Pages/SqlEditor.jsx
+++ src/Pages/SqlEditor.jsx
@@ -8,6 +8,8 @@ import { sendGetRequest, sendPostRequest, API_HOSTNAME } from '../Hooks/useApiRe
 
 import { Container, Typography } from '@mui/material';
 
+import DataTable from 'react-data-table-component';
+
 import './CSS/SqlEditor.css';
 
 function Question({ question, setQuestion, history, setHistory, setExpectedResult, setActualResult, result, setResult }) {
@@ -41,7 +43,7 @@ function Question({ question, setQuestion, history, setHistory, setExpectedResul
 				sx={{
 					textAlign: 'center',
 				}}>
-				<div style={{'font-size': '1.25rem'}}>{question.question}</div>
+				<div style={{'fontSize': '1.25rem'}}>{question.question}</div>
 
 				<Typography variant="body1">{
 					typeof result === 'string' ? result
@@ -57,43 +59,18 @@ function ResultTable({ results }) {
 	if (typeof results !== 'object') return <div>{results}</div>;
 	else if (!('length' in results) || results.length === 0) return <div></div>;
 
-	// If the user enters multiple queries, the backend will return an array of results.
-	// SELECT statements return arrays of rows, but others (e.g. CREATE TABLE) may return numbers.
-	for (let element of results) {
-		if (element instanceof Array) {
-			// If the results array contains subarrays, display each element as a separate result.
-
-			const returnValue = [];
-			for (let currentElement of results) {
-				returnValue.push(<ResultTable results={currentElement}/>);
-			}
-
-			return returnValue;
+	const columns = Object.keys(results[0]).map(col => {
+		return {
+			name: col,
+			selector: row => row[col],
+			sortable: true,
 		}
-	}
+	});
 
-	const columns = [];
-	for (let key in results[0]) columns.push(key);
-
-	const width = `${100 / columns.length}%`;
-
-	let columnTh = [];
-	for (let val of columns) columnTh.push(<td style={{width: width}}>{val}</td>);
-
-	let rows = [];
-	for (let row of results) {
-		let columnData = [];
-
-		for (let key of columns) columnData.push(<td>{row[key]}</td>);
-		rows.push(<tr>{columnData}</tr>);
-	}
-	
-	return <table className='results-table' style={{width: "100%"}}>
-		<thead>
-			<tr>{columnTh}</tr>
-		</thead>
-		<tbody>{rows}</tbody>
-	</table>;
+	return <DataTable
+		columns={columns}
+		data={results}
+	/>;
 }
 
 function SqlEditor() {
@@ -137,7 +114,7 @@ function SqlEditor() {
 	return (
 		<>
 			<div className='solid-background'></div>
-			<div style={{'margin-bottom': '150px'}}></div>
+			<div style={{'marginBottom': '150px'}}></div>
 			<div>
 				<div>
 					<Suspense fallback={<Loader />}>
@@ -151,7 +128,7 @@ function SqlEditor() {
 							result={result}
 							setResult={setResult}
 						/>
-						<div style={{ 'margin-bottom': '16px' }}></div>
+						<div style={{ 'marginBottom': '16px' }}></div>
 						<Editor
 							value={query}
 							setValue={setQuery}
@@ -171,7 +148,7 @@ function SqlEditor() {
 								Clear history
 							</button>
 							<div style={{'float': 'right'}}>
-								<button class="primary-btn" onClick={submitQuery}>
+								<button className="primary-btn" onClick={submitQuery}>
 									Submit query
 								</button>
 
@@ -190,13 +167,13 @@ function SqlEditor() {
 						<table style={{width: "100%"}}>
 							<thead>
 								<tr>
-									<th style={{width: "50%", "border-right": "1px solid black"}}>Expected Results</th>
+									<th style={{width: "50%", "borderRight": "1px solid black"}}>Expected Results</th>
 									<th style={{width: "50%"}}>Actual Results</th>
 								</tr>
 							</thead>
 							<tbody>
 								<tr>
-									<td style={{"border-right": "1px solid black"}}><ResultTable results={expectedResult}/></td>
+									<td style={{"borderRight": "1px solid black"}}><ResultTable results={expectedResult}/></td>
 									<td><ResultTable results={actualResult}/></td>
 								</tr>
 							</tbody>
